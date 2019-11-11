3b0cb713b657cf13f6e985904183d952a27cc727
eric <Eric355402@yahoo.com>
2019-11-11 11:49:19 -0700

    completed mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,6 +5,17 @@ import App from './App';
 
 test('renders without crashing', () => {
   //Arrange, Act, Assert
-  const container = render(<App />);
-  console.log(container);
+  render(<App />);
 });
+
+test('it displays a link button with the text Switch theme', () => {
+  //Arrange, Act, Assert
+  const { getByText } = render(<App />);
+  getByText('Switch theme');
+});
+
+// test('it displays google searches text', () => {
+//   //Arrange, Act, Assert
+//   const { getByText } = render(<App />);
+//   getByText('');
+// });
